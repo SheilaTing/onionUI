950715e96475d24df08004992c00b318d80c1bd4
sheilaTing <835113231@qq.com>
2020-07-29 16:41:52 +0800

    chai 单元测试

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Button from './button.vue';
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
+import { expect } from "chai";
 
 Vue.component('o-button',Button)
 Vue.component('o-icon',Icon)
@@ -13,3 +14,108 @@ new Vue({
         loading2:false
     }
 })
+
+//使用 chai 测试
+// 对组件的每个入参都要做测试
+//第一个
+{
+    //获取 Button 组件的内容
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        // 测试组件的参数部分，如下方式传参 文档有写
+        propsData:{
+            icon:'setting'
+        }
+    });
+    button.$mount() // 将这个按钮挂载到页面测试 也可以不传递dom,不报错就通过了
+    //测试 icon 图标，获取 use标签
+    let useElement = button.$el.querySelector('use')
+    //因为传入了 setting,所以期待标签里的 href 等于 setting
+   expect( useElement.getAttribute('xlink:href')).to.eq('#icon-setting')
+   button.$el.remove()
+   button.$destroy()
+}
+//测试 loading参数，期待传入后，setting不显示
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true
+        }
+    });
+    button.$mount('#app') // 将这个按钮挂载到页面测试 也可以不传递dom,不报错就通过了
+    //测试 icon 图标，获取 use标签
+    let useElement = button.$el.querySelector('use')
+    //因为传入了 setting,所以期待标签里的 href 等于 setting
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+    button.$el.remove()
+    button.$destroy()
+}
+//测试 position
+{
+    const div = document.createElement('div');
+    document.body.append(div);
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true,
+            //未传递 iconposition 默认order 1
+
+        }
+    });
+    button.$mount(div) // 将这个按钮挂载到页面测试 也可以不传递dom,不报错就通过了
+    //测试 positon 获取 图标的order
+    let svg = button.$el.querySelector('svg')
+    //获取 svg的样式,获取设置的order
+    let {order} = window.getComputedStyle(svg);
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq("1")
+    button.$el.remove()
+    button.$destroy()
+}
+    //传递 position
+{
+    const div = document.createElement('div');
+    document.body.append(div);
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true,
+            iconposition:'right'
+
+        }
+    });
+    button.$mount(div) // 将这个按钮挂载到页面测试 也可以不传递dom,不报错就通过了
+    //测试 positon 获取 图标的order
+    let svg = button.$el.querySelector('svg')
+    //获取 svg的样式,获取设置的order
+    let {order} = window.getComputedStyle(svg);
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq("2")
+    //成功后，删除mount到页面的 内容
+    button.$el.remove()
+    button.$destroy()
+}
+//测试点击事件
+{
+    const Constructor = Vue.extend(Button);
+    let obutton = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true,
+            iconposition:'right'
+        }
+    });
+    obutton.$mount();
+    obutton.$on('click',fucntion(){
+        
+    })
+    //找到组件中的button
+    let button = obutton.$el;
+    //触发事件
+    button.click();
+}
