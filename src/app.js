689204850e24ae7fe5d026ddf7c14301b38a1ffd
sheilaTing <835113231@qq.com>
2020-07-29 11:57:50 +0800

    添加icon 和icon-position,控制按钮图标样式

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Button from './button.vue';
+import Icon from './icon.vue'
 
 Vue.component('o-button',Button)
+Vue.component('o-icon',Icon)
 new Vue({
     el:'#app'
 })
--- src/button.vue
+++ src/button.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        <button class='o-button'>按钮</button>
+        <button class='o-button' :class="{ [`icon-${iconPosition}`]: true }">
+            <o-icon icon="`${icon}`"></o-icon>
+           <div class="content">
+                <slot></slot>
+           </div>
+        </button>
     </div>
 </template>
 
 <script>
     export default {
-        
+        props:['icon','iconPosition']
     }
 </script>
 
@@ -19,6 +24,18 @@
         border-radius:var(--border-radius);
         border:1px solid var(--border-color);
         background:var(--button-bg);
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align:middle;
+        >.icon{
+            // 图标默认order:1 左侧
+            order:1;  
+            margin-right: .3em;
+        }
+        >.contnet{
+            order:2;  // button 内容在右侧
+        }
         
         &:hover{
             border-color: var(--border-color);
@@ -28,6 +45,25 @@
         }
         &:active{
             background-color: var(--button-active-bg);
+        }
+        &.icon-left{
+            >.icon{
+                order:1;
+                margin-right: .3em;
+            }
+            >.content{
+                order:2;
+            }
+        }
+        &.icon-right{
+            >.icon{
+                order:2;
+                margin-left: .3em;
+                margin-right: 0; // 发现右侧也有空隙，去除
+            }
+            >.content{
+                order:1
+            }
         }
      }
 </style>
